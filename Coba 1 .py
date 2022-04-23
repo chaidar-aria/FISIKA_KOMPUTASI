fd4b2194a8eb5d249550dd1b03f829c0f24e2138
minervaarcane <rianaoktafianti.21040@mhs.unesa.ac.id>
2022-04-23 12:22:03 +0700

    Step 1

---
+++ Coba 1 .py
@@ -0,0 +1,10 @@
+import math
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import *
+
+data = pd.read_csv("T1.1.csv")
+data.head()
